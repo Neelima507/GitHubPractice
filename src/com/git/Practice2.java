cbe894c63188df4264538b251180645f59574b9c
Neelima <neelutgd507@gmail.com>
2025-08-30 15:56:02 -0700

    added new class

--- src/com/git/Practice2.java
+++ src/com/git/Practice2.java
@@ -2,9 +2,11 @@ package com.git;
 
 
 
-public class Practice {
-	public void newmethod() {
-		
+public class Practice2 {
+	public void newMethod() {
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("AGAIN");
 	}
 		
 	
